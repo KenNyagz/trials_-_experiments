b7540b81779c871e79f49653c8d0a05b287f75a7
KenNyagz <kennyagz681@gmail.com>
2024-04-21 21:28:03 +0300

    Some basic C functionality + barebones website with only html css

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n[3] = {56,57 ,58};
+    int (*p)[3];
+    p = &n;
+    printf("%p\n", *p);
+    printf("%p\n", &n[0]);
+    for (int i = 0; i < 3; i++)
+    printf("%d ", n[i]);
+    putchar('\n');
+    p++;
+    printf("%p\n", p);
+    return (0);
+    
+}
