bbf5a475af46579acfe797ddf3d435882b19dbcf
KenNyagz <kennyagz681@gmail.com>
2024-04-29 22:19:02 +0300

    Custom power function status

--- cust_exp.c
+++ cust_exp.c
@@ -4,25 +4,24 @@
 
 long long powr(long long base, int exp)
 {
-int i;
-long long result = 1;
-for (i=0; i<exp; i++)
-{
-result = (result * base);
-
-}
-return result;
+	int i;
+	long long result = 1;
+	for (i=0; i<exp; i++)
+	{
+		result = (result * base);
+	}
+	return result;
 }
 
 int main()
 {
-//message = 4, n = 2*3=6
-long long enc = powr(7, 5)%6;
+	//message = 4, n = 2*3=6
+	long long enc = powr(7, 5)%6;
 
-printf("%lld..\n",enc);
+	printf("%lld..\n",enc);
 
-long long dec = powr(enc,11)%6;
+	long long dec = powr(enc,11)%6;
 
-printf("%lld\n",dec);
-return 0;
+	printf("%lld\n",dec);
+	return 0;
 }
