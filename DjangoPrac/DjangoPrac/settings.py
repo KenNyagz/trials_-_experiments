02bcd901692d8e38f280bfbbd20e7e507ad5c9ad
KenNyagz <kennyagz681@gmail.com>
2024-09-24 22:23:53 +0300

    Change from SQLite to postgres on aws-RDS

--- DjangoPrac/DjangoPrac/settings.py
+++ DjangoPrac/DjangoPrac/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoPrac.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Babayega01',
+        'HOST': 'django-prac.czi6g8eggmcx.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
