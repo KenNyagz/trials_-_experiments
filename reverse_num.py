5ac50b41d8f1218d0e8705da60c8ee6890dfbe7c
KenNyaga <kennyagz681@gmail.com>
2024-10-28 10:09:06 +0300

    Simple mock interview with an old friend

---
+++ reverse_num.py
@@ -0,0 +1,19 @@
+You have the numbers 12345, 9845, 178999884662, 7834865
+Write a function to reverse the numbers 
+example 12345 to 54321
+
+num1 = 12345
+num2 = 9845
+num3 = 178999884662
+num4 = 7834865
+
+def reverse_num(int):
+    '''Function that reverses a number'''
+    num_str = str(num)
+
+    for 
+A:wq
+
+A
+    
+
