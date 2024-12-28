35ba0a4327ddff0946c14de6c34cf3dedad9cf49
KenNyagz <kennyagz681@gmail.com>
2024-12-28 13:39:36 +0300

    Twilio for sending SMS text messages

--- twilio_text_send.py
+++ twilio_text_send.py
@@ -1,16 +1,15 @@
 from twilio.rest import Client
 
 # Twilio credentials
-account_sid = 'AC13f1d53cd3b04701e0e0fccd991ab2f3'
+account_sid = ''
 auth_token = ''
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body="Activate",
-    from_='+254103654277',
-    #from_='+254717044108',  # Your Twilio number
-    to='+19723609222'      # Recipient's American number
+    from_='',  # Your Twilio number
+    to=''      # Recipient's American number
 )
 
 print(f"Message sent with SID: {message.sid}")
