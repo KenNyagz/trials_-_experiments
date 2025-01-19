5707e368abbd84302f2bae54c940c6ffd34f682f
Ken Nyaga <112327222+KenNyagz@users.noreply.github.com>
2025-01-19 20:59:36 +0300

    Edit to include conversion of RGBA pictures to RGB.
    Convert the image to RGB mode before saving it as a PDF

--- image_to_pdf_converter.py
+++ image_to_pdf_converter.py
@@ -3,4 +3,7 @@ from PIL import Image
 # You need a libray called pillow - "pip install Pillow"
 image = Image.open('../sample_cv.jpg') # can read jpgs, pngs and other image formats
 
+#Pillow library does not support saving images in the RGBA mode to a PDF. PDF files typically require images in RGB mode.
+image = image.convert("RGB") # Convert the image to RGB mode(for those in RGBA), this may not be necessary all the time though
+
 image.save('vwalla.pdf') # save output to file(pdf), with 'pdf' extension
